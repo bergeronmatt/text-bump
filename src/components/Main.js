c943050bc34c0b6136b2006558276dca5e761ca0
ranraok <46853842+ranraok@users.noreply.github.com>
2021-06-20 19:50:34 -0400

    changed time

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 
 export default function Main() {
 
-    const [seconds, setSeconds] = useState(11);
+    const [seconds, setSeconds] = useState(6);
 
     useEffect(() => {
         if (seconds > 0) {
-            setTimeout(() => setSeconds(seconds-1), 3000);
+            setTimeout(() => setSeconds(seconds-1), 3500);
         } else {
             setSeconds(0)
         }
@@ -30,16 +30,11 @@ export default function Main() {
             {seconds}
         </div>
       <div className="text-container">
-        <p id="11" className="cards hidden">Hi, my name's Matt</p>
-        <p id="10" className="cards hidden">I'm a Full Stack Web Developer</p>
-        <p id="9" className="cards hidden">I like to make web sites and apps</p>
-        <p id="8" className="cards hidden">I even made this whole <br/>tik-tok in code</p>
-        <p id="7" className="cards hidden">I'm really good at making eCommerce sites without things like Shopify</p>
-        <p id="6" className="cards hidden">because I don't like how it costs businesses so much over time</p>
-        <p id="5" className="cards hidden">I lost my job at a warehouse recently</p>
-        <p id="4" className="cards hidden">so I'm trying to find a full time development job,</p>
-        <p id="3" className="cards hidden">or find freelance work until I do. So if you know someone who needs a site</p>
-        <p id="2" className="cards hidden">or is hiring, I'd appreciate if you'd send them my way or share this tik-tok.</p>
+        <p id="6" className="cards hidden">Hi, my name's Matt. I'm a Full Stack Web Developer</p>
+        <p id="5" className="cards hidden">I like to make web sites and apps; I even made this tik-tok in code</p>
+        <p id="4" className="cards hidden">I'm really good at making eCommerce sites without things like Shopify because I don't like their business model.</p>
+        <p id="3" className="cards hidden">I recently lost my warehouse job, so I'm trying to find a full time development job, or find freelance work until I do.</p>
+        <p id="2" className="cards hidden">So if you know someone who needs a site or is hiring, I'd appreciate if you'd send them my way or share this tik-tok.</p>
         <p id="1" className="cards hidden">Thanks {`<3`}</p>
         <p id="0" className="cards hidden">[as]</p>
       </div>
